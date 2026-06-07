98999146d407731f427711046bc41beb847ffaec
Win Rawin <rawin@belusys.com>
2026-06-07 22:01:13 +0700

    update code

--- build.bat
+++ build.bat
@@ -8,8 +8,8 @@ VERSION=${1:-latest}
 docker buildx create --use --name multi-builder
 
 # build multi-platform (amd64 + arm64) แล้ว push ไป Docker Hub ในคำสั่งเดียว
-docker buildx build \
 #  --platform linux/amd64,linux/arm64 \
+docker buildx build \
   --platform linux/amd64 \
   --build-arg VERSION_NUMBER=$VERSION \
   -t $IMAGE:$VERSION \
